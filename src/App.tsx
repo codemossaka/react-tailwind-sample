3c3fd16d00e95e0b09aa2f98740e59da909749fa
Essonga Godson <peyagodson@ymail.com>
2022-10-27 03:19:14 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className=" w-full flex justify-between rounded" >
+            <div className="border w-[500px] h-[200px] rounded bg-blue-200">
+                <button className="square">
+                    click
+                </button>
+            </div>
+            <div className="border w-[500px] h-[200px] rounded bg-orange-200">
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
